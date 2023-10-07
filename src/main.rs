8167979fe174c61401f399fe0d5ad16104144858
Gustavo Vasquez <gustavovasquez2002@gmail.com>
2023-10-07 01:33:07 -0300

    feat: hello world from rocket

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::http::Status;
+use rocket::response::{content, status};
+
+#[macro_use] extern crate rocket;
+
+#[get("/hello-world")]
+fn index() -> status::Custom<content::RawJson<&'static str>> {
+    status::Custom(
+        Status::Ok,
+        content::RawJson("{\"message\": \"Hello, world!\"}"),
+    )
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
